2a400c3357d20be9ef4fcaa84c298fc0396a536e
Renata Inacio <revazgauska@gmail.com>
2023-05-15 05:40:13 -0300

    pre-jogo

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import { useState } from 'react'
 
 export default function App() {
   const [game, setGame] = useState(0);
+  const [erros, setErros] = useState([]);
+  const [acertos, setAcertos] = useState([]);
+  const [palavra, setPalavra] = useState("");
   return (
     <div>
-      <Jogo game={game} setGame={setGame}/>
+      <Jogo game={game} setGame={setGame} palavra={palavra} setPalavra={setPalavra} acertos={acertos} setAcertos={setAcertos} erros={erros} setErros={setErros}/>
     </div>
   );
 }
--- src/Jogo.js
+++ src/Jogo.js
@@ -1,16 +1,36 @@
 import palavras from './palavras.js';
 import imagemForca0 from './assets/forca0.png'
+import imagemForca1 from './assets/forca1.png'
+import imagemForca2 from './assets/forca2.png'
+import imagemForca3 from './assets/forca3.png'
+import imagemForca4 from './assets/forca4.png'
+import imagemForca5 from './assets/forca5.png'
+import imagemForca6 from './assets/forca6.png'
 import Letras from './Letras'
 
 export default function Jogo(props) {
-    console.log(palavras);
+    const {palavra, setPalavra, acertos, setAcertos, game, setGame, erros, setErros} = props;
+    const imagemForca = [imagemForca0, imagemForca1, imagemForca2, imagemForca3, imagemForca4, imagemForca5, imagemForca6];
+    const numeroErros = erros.length;
+    function comparador() { 
+        return Math.random() - 0.5; 
+    }
+    function iniciarJogo() {
+        setGame(1);
+        setPalavra(palavras.sort(comparador)[0]);
+    }
     return (
       <div>
         <div className='main'>
-            <img src={imagemForca0}/>
-            <button onClick={() => props.setGame(1)}>Escolher Palavra</button>
+            <img src={imagemForca[{numeroErros}]}/>
+            <div className='botao-palavra'>
+                <button onClick={iniciarJogo} >Escolher Palavra</button>
+                <ul className='palavra'>{palavra.split("").map((letra, indice) =>
+                    <li key={indice}>{acertos.includes(letra) ? letra : '_  '}</li>
+                    )}</ul>
+            </div>
         </div>
-        <Letras game={props.game}/>
+        <Letras game={game}/>
       </div>
     );
 }
